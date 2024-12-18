0d8767ef71d70e82dec081878b4e6a8e33445d14
GregoryVDev <arexo@hotmail.fr>
2024-12-18 19:17:01 +0100

    Added comments for the example

--- test.py
+++ test.py
@@ -4,7 +4,12 @@ def main():
     username = "Avo"
     # Création d'une variable "age" ayant pour valeur "29"
     age = 29
+    # Affiche le username et l'âge
     print(username, age)
+    # Change la valeur par 30
+    age = 30
+    # Affiche la nouvelle valeur de l'âge
+    print(age)
 
 
 if __name__ == '__main__':
