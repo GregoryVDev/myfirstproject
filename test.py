041c4fefabbe58cf01c8fde6943a61c8a516fbff
GregoryVDev <arexo@hotmail.fr>
2024-12-18 19:13:05 +0100

    Added username display

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 
 def main():
-    print("Hello Avo")
-    print("Salut à tous !")
+    # Création d'une variable "username" ayant pour valeur "Je suis nouveau sur Python !"
+    username = "Je suis nouveau sur Python !"
+    # Création d'une variable "age" ayant pour valeur "29"
+    age = 29
+    print(username)
 
 
 if __name__ == '__main__':
