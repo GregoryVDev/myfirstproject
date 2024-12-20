b984c8f9ddc566538a01450ba822b5683d249afc
GregoryVDev <arexo@hotmail.fr>
2024-12-20 16:37:16 +0100

    Edited the name file

---
+++ exo.py
@@ -0,0 +1,15 @@
+
+def main():
+    # Récolter une valeur porte monnaie
+    wallet = 101
+    print("Votre porte monnaie est de " + str(wallet) + "€")
+    # Créer un produit qui aura pour valeur 50
+    product = 50
+    print("Le produit que vous voulez acheter est de " + str(product) + "€")
+    # Afficher la nouvelle valeur du porte monnaie, après son achat
+    result = (wallet - product)
+    print("Apès votre achat de " + str(product) + " Votre porte monnaie restant est de " + str(result) + "€")
+
+
+if __name__ == '__main__':
+    main()
