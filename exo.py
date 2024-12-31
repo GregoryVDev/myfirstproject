8c01afcee1127d275b8830daf36323094c4351b2
GregoryVDev <arexo@hotmail.fr>
2024-12-31 14:45:02 +0100

    Conditions for checking your age and your partner for exercise

--- exo.py
+++ exo.py
@@ -1,17 +1,18 @@
 
 def main():
     # Faire un input et le stocker dans la variable note
-    note = float(input("Veuillez saisir votre note de mathématiques"))
-    if note >= 16:
-        print("Très bien")
-    elif note >= 14:
-        print("Bien")
-    elif note >= 12:
-        print("Assez bien")
-    elif note >= 10:
-        print("Votre travail est moyen")
+    age = float(input("Veuillez saisir votre age:"))
+    woman_age = float(input("Veuillez saisir l'âge de votre partenaire:"))
+
+
+    if age <= 30:
+        if woman_age < age:
+            print("Vous avez moins de 30 ans et votre partenaire est moins agé que vous!")
+        else:
+            print("Vous avez moins de 30 ans mais votre partenaire est plus agé que vous!")
     else:
-        print("Vous devez faire un effort")
+        print("Votre age est supérieur à 30")
+
 
 if __name__ == '__main__':
     main()
