ddb54a9f3c18abeafbfb1bcde8f4b007ea61c7a6
GregoryVDev <arexo@hotmail.fr>
2024-12-31 15:27:25 +0100

    Exercise on multiple conditions and logical operators (update)

--- exo.py
+++ exo.py
@@ -1,16 +1,12 @@
 
 def main():
-    # Faire un input et le stocker dans la variable note
-    age = float(input("Veuillez saisir votre age:"))
-    woman_age = float(input("Veuillez saisir l'âge de votre partenaire:"))
 
+    a, b, c = 5,8,0
 
-a, b, c = 5,8,0
-
-if(a<c and b<c):
-    print("true")
-else:
-    print("false")
+    if(a<c and b<c):
+        print("true")
+    else:
+        print("false")
 
 
 if __name__ == '__main__':
