b50b25d815cfd9d238e221aa732a429d49202c01
GregoryVDev <arexo@hotmail.fr>
2025-01-02 22:43:50 +0100

    Displayed two values in the list

--- exo.py
+++ exo.py
@@ -3,8 +3,7 @@ def main():
     # Ajouter et mettre à jour les éléments d'une liste
     my_list = [1,2,"Amine","Martin",-5,False]
 
-    my_list[3] = "Assude"
-    print(my_list[3])
+    print(my_list[1:3])
 
 
 if __name__ == '__main__':
