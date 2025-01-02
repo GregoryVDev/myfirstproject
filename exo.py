c0b8242445a5b178a5137dcee1444f387b51acc5
GregoryVDev <arexo@hotmail.fr>
2025-01-02 21:32:58 +0100

    Added a continue in the for loop

--- exo.py
+++ exo.py
@@ -1,14 +1,13 @@
 
 def main():
     # Le mot clé break et continue
-    for i in range (10):
-        print("La valeur de i est: ", i)
-        print("Python")
-        if i==4:
-            break
-        print("Fin de l'iteration à cause de la valeur de i qui vaut: ",i)
-
-        print("Fin de la boucle")
+    for i in range(7):
+        print("Début de la boucle numéro :", i)
+        print("Bienvenue dans la formation Python ! :")
+        if i < 4:
+            continue
+        print("La valeur de i est supérieur ou égale à:", i)
+    print("fin de la boucle")
 
 
 if __name__ == '__main__':
