af281ab681dcc6877f768657b988c606d8f28b15
GregoryVDev <arexo@hotmail.fr>
2025-01-03 15:03:31 +0100

    Deleted the list element

--- exo.py
+++ exo.py
@@ -1,9 +1,10 @@
 
 def main():
-    # Ajouter et mettre à jour les éléments d'une liste
+    # Supprimer les éléments d'une liste
     my_list = [1,2,"Amine","Martin",-5,False]
 
-    print(my_list[1:3])
+    del my_list[0]
+    print(my_list)
 
 
 if __name__ == '__main__':
