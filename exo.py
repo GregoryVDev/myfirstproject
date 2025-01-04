4fed8de65d4e5d427fc8b5103f6bea00773a3f13
GregoryVDev <arexo@hotmail.fr>
2025-01-04 16:44:21 +0100

    Created the tuple emptu

--- exo.py
+++ exo.py
@@ -1,9 +1,7 @@
 
 def main():
     # Récupérer les éléments d'un tuple
-    tuple2 = ("Amine", 40, "Karine", [1,2,3,4], ("a","b","c","d"))
-
-    print(tuple2[3][2])
+    t = ()
 
 
 if __name__ == '__main__':
