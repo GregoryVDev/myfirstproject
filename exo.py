7aef5cb268dbd711ceaeb03d417c945a5cac1ca6
GregoryVDev <arexo@hotmail.fr>
2025-01-04 16:41:22 +0100

    Display the first, third and fourth results in the tuple

--- exo.py
+++ exo.py
@@ -1,10 +1,11 @@
 
 def main():
-    # Les méthodes split() et join()
-    capitales = ["Madrid", "Paris", "Geneve", "Rabat", "Lisbonne"]
-    my_chain = "-".join(capitales)
+    # Récupérer les éléments d'un tuple
+    tuple2 = ("Amine", 40, "Karine", [1,2,3,4], ("a","b","c","d"))
 
-    print(my_chain)
+    print(tuple2[0])
+    print(tuple2[3])
+    print(tuple2[4])
 
 
 if __name__ == '__main__':
