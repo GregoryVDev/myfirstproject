5059323e528da988c2fc6b3c5bebf55470501fa6
GregoryVDev <arexo@hotmail.fr>
2025-01-04 17:11:20 +0100

    Added the int function to remove digits after the decimal point

--- exo.py
+++ exo.py
@@ -2,11 +2,12 @@
 def main():
     # Fonction prédéfinies en Python et accessibles directement
     x = -34
+    m = 12.0
     y = 645.6
     z = 67
     t = -32
 
-    print(float(x))
+    print(int(y))
 
 
 if __name__ == '__main__':
