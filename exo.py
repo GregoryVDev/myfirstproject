d11b6e900a196ea190bccd995bf940bb733f53d5
GregoryVDev <arexo@hotmail.fr>
2025-01-04 16:42:03 +0100

    Display the number 3 in array

--- exo.py
+++ exo.py
@@ -3,9 +3,7 @@ def main():
     # Récupérer les éléments d'un tuple
     tuple2 = ("Amine", 40, "Karine", [1,2,3,4], ("a","b","c","d"))
 
-    print(tuple2[0])
-    print(tuple2[3])
-    print(tuple2[4])
+    print(tuple2[3][2])
 
 
 if __name__ == '__main__':
