692b18f0c51a9ddd95405d1128b2f0af37f92e6c
GregoryVDev <arexo@hotmail.fr>
2025-01-06 19:04:10 +0100

    Retrieve a person's input value

--- exo.py
+++ exo.py
@@ -1,6 +1,7 @@
-# Ajouter de nouvelles entrées à un dictionnaire
+# Accès aux entrées d'un dictionnaire
 
 note = { "Lionel": 13.5, "Amine": 14, "Lina": 17, "Pierre": 20}
-note["Karine"] = 19
+
+print(note["Amine"])
 
 print(note)
