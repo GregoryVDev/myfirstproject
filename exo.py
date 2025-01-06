31b68fa3bafc65714bded49fda6859e87891052a
GregoryVDev <arexo@hotmail.fr>
2025-01-06 19:00:02 +0100

    Added a person to the dictionary

--- exo.py
+++ exo.py
@@ -1,5 +1,6 @@
-# Les dictionnaires
+# Ajouter de nouvelles entrées à un dictionnaire
 
 note = { "Lionel": 13.5, "Amine": 14, "Lina": 17, "Pierre": 20}
+note["Karine"] = 19
 
-print(type(note))
+print(note)
