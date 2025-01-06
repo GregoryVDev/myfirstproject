0c775f23a03246f664a30b6ef4897471fe258846
GregoryVDev <arexo@hotmail.fr>
2025-01-06 19:14:46 +0100

    Displayed in list number 2

--- exo.py
+++ exo.py
@@ -5,4 +5,4 @@ note = { "Lionel": 13.5, "Amine": 14, "Lina": 17, "Pierre": 20}
 dic = {"prenom":"Amine","list":[1,2,3], "tp":(10,20,30,40), 1:"Python", "capitales":{"France":"Paris","Espagne":"Madrid"}}
 
 print(dic["prenom"])
-print(dic["list"])
+print(dic["list"][1])
