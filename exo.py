a9aa88d2f175b05c58ac8381c73661b6e8f7dac9
GregoryVDev <arexo@hotmail.fr>
2025-01-06 19:35:46 +0100

    Added the variable notes to the dictionary which has the variable dic

--- exo.py
+++ exo.py
@@ -1,10 +1,6 @@
-# Accès aux entrées d'un dictionnaire
+# Mettre à jour les entrées d'un dictionnaire
 
-note = { "Lionel": 13.5, "Amine": 14, "Lina": 17, "Pierre": 20}
+notes = { "Lionel": 13.5, "Amine": 14, "Lina": 17, "Pierre": 20}
 
-dic = {"prenom":"Amine","list":[1,2,3], "tp":(10,20,30,40), 1:"Python", "capitales":{"France":"Paris","Espagne":"Madrid"}}
-
-
-print(dic["list"][1])
-print(dic["tp"][3])
-print(dic["capitales"]["France"])
+dic = {(1,2,3):"Amine", "notes": notes, "list":[1,2,3], "tp":(10,20,30,40), 1:"Python", "capitales":{"France":"Paris","Espagne":"Madrid"}}
+print(dic)
