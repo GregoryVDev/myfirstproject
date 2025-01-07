547f9be091dc5ba6ed58e5b470de27cb0f78f988
GregoryVDev <arexo@hotmail.fr>
2025-01-07 17:43:59 +0100

    Removed "Lionel" from the dictionary

--- exo.py
+++ exo.py
@@ -1,6 +1,6 @@
-# Mettre à jour les entrées d'un dictionnaire
+# Supprimer les entrées d'un dictionnaire
 
 notes = { "Lionel": 13.5, "Amine": 14, "Lina": 17, "Pierre": 20}
 
-dic = {(1,2,3):"Amine", "notes": notes, "list":[1,2,3], "tp":(10,20,30,40), 1:"Python", "capitales":{"France":"Paris","Espagne":"Madrid"}}
-print(dic)
+del(notes["Lionel"])
+print(notes)
