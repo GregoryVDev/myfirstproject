2a1e8a13f9c5faba5ffb57b75163c620715a33c3
GregoryVDev <arexo@hotmail.fr>
2025-01-09 22:16:50 +0100

    Added utf-8 encoding to be able to read special or unrecognised characters

--- exo.py
+++ exo.py
@@ -1,5 +1,5 @@
 # Méthode simple et élégante pour lire un fichier
-notre_poeme = open("poeme.txt", "r")
+notre_poeme = open("poeme.txt", "r", encoding="utf-8")
 
 for line in notre_poeme:
     print(line)
