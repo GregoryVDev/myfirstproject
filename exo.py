7f599dac7ab9c5292b5bc271dd0f39a828b42d2d
GregoryVDev <arexo@hotmail.fr>
2025-01-10 16:27:50 +0100

    Displayed in the file test_file.txt a new message with file.write

--- exo.py
+++ exo.py
@@ -3,3 +3,4 @@
 # Créer un nouveau fichier dans le repertoire du dossier
 fichier = open('test_file.txt', 'w', encoding="UTF-8")
 fichier.write("Salut mes camarades \n")
+fichier.write("Nous sommes en train d'apprendre Python tout doucement \n")
