c1d12e041882479b735c83de26d75a5cebfc3351
GregoryVDev <arexo@hotmail.fr>
2025-01-10 16:32:28 +0100

    Open the file again with ‘w’, the file becomes a new file and is empty.

--- exo.py
+++ exo.py
@@ -9,3 +9,6 @@ fichier.write("Nous sommes en train d'apprendre Python tout doucement \n")
 
 # Fermer le fichier
 fichier.close()
+
+# Ouvrir de nouveau le fichier test_file (le fichier est vide car il a remplacé ce qu'on a écrit par un nouveau fichier vide
+fichier = open('test_file.txt', 'w', encoding="UTF-8")
