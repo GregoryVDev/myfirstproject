d42e7144809c77840e6e4b6dd8c5fa7445f5a830
GregoryVDev <arexo@hotmail.fr>
2025-01-10 16:04:57 +0100

    Displayed and read with the for loop each line in the "notre_poeme" alias

--- exo.py
+++ exo.py
@@ -1,5 +1,7 @@
-# Méthode simple et élégante pour lire un fichier
-notre_poeme = open("poeme.txt", "r", encoding="utf-8")
+# Ouverture et fermeture automatique d'un fichier
 
-for line in notre_poeme:
-    print(line)
+# Ouvrir poeme.txt avec le mode par défaut 'r' et l'encoder UTF-8 avec un alias "notre_poeme"
+with open('poeme.txt', encoding="UTF-8") as notre_poeme:
+    # Permet de faire une boucle pour lire chaque ligne dans la variable notre_poeme
+    for line in notre_poeme:
+        print(line)
