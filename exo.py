2da5ff344d203a44ad04195c218287cff4a986d4
GregoryVDev <arexo@hotmail.fr>
2025-01-10 16:29:39 +0100

    Added the commentary and closed the file with "fichier.close()"

--- exo.py
+++ exo.py
@@ -2,5 +2,10 @@
 
 # Créer un nouveau fichier dans le repertoire du dossier
 fichier = open('test_file.txt', 'w', encoding="UTF-8")
+
+# Ecrire deux messages dans le fichier test_file.txt
 fichier.write("Salut mes camarades \n")
 fichier.write("Nous sommes en train d'apprendre Python tout doucement \n")
+
+# Fermer le fichier
+fichier.close()
