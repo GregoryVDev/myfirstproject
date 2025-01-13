37d4111ef2c6b6e9a0984fb04b442659855441ef
GregoryVDev <arexo@hotmail.fr>
2025-01-13 16:43:48 +0100

    Posted today's date

--- exo.py
+++ exo.py
@@ -2,8 +2,5 @@
 
 from datetime import date
 
-# Afficher la date entière
-date = date(1995, 12, 10)
-
-format = date.strftime("%d/%m/%y")
-print("Ma date de naissance est", format)
+today = date.today()
+print(today)
