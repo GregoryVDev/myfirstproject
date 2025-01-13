ab4120b16bc65d8a67096aefadd483e8f5eb3384
GregoryVDev <arexo@hotmail.fr>
2025-01-13 19:42:15 +0100

    Displayed the type of error that corresponds

--- exo.py
+++ exo.py
@@ -1,10 +1,14 @@
-# Comment éviter que le programme se plante
+# Comment préciser le type des erreurs
 def inverse(x):
     y = 1.0/x
     return y
 
 try:
     x = float(input("Donnez un nombre que vous souhaitez avoir son inverse:"))
-    print(inverse(x))
-except:
+    y = inverse(x)
+except Exception as e:
     print("Ce que vous avez saisi n'admet pas d'inverse")
+    # Indiquer un message avec un type d'erreur qui correspond
+    print("Le type d'erreur est:", e.__class__)
+else:
+    print(y)
