34a9ba113da7b788dfc631be52641572ec488d08
GregoryVDev <arexo@hotmail.fr>
2025-01-15 17:44:58 +0100

    Creates an instantiated class with employee information and creates an employee1 object by adding the information for employee1

--- Employe.py
+++ Employe.py
@@ -1,30 +1,15 @@
 # Instanciation des classes
 class Employe:
-    name = ""
-    firstname = ""
-    age = 0
-    phone = ""
-    email = ""
+    def __init__(self, name, firstname, age, email):
+        self.employe_name = name
+        self.employe_firstname = firstname
+        self.employe_age = age
+        self.employe_email = email
 
     def login(self):
         print("J'ai besoin de me connecter")
 
     def logout(self):
-        print("J'ai terminé et j'ai besoin de me déconnecter")
+        print("J'ai besoin de me déconnecter")
 
-employe1 = Employe()
-employe2 = Employe()
-
-employe1.name = "Doe"
-employe1.firstname = "John"
-employe1.age = 40
-
-employe2.name = "Smith"
-employe2.firstname = "Leon"
-employe2.age = 29
-
-print(employe1.name, employe1.firstname, "a pour âge ", employe1.age)
-print(employe2.name, employe2.firstname, "a pour âge ", employe2.age)
-
-employe1.login()
-employe1.logout()
+employe1 = Employe("Smith", "Joe", 40, "joe@gmail.fr")
