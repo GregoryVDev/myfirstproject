6365032440cf3ceb690c46369b469a7a4f62548a
GregoryVDev <arexo@hotmail.fr>
2025-01-15 16:01:24 +0100

    Defining a class in object-oriented programming with Python and create employe file

---
+++ Employe.py
@@ -0,0 +1,16 @@
+# Programmation orientée objet
+
+# Définir une class avec Python
+
+class Employe:
+    name = ""
+    firstname = ""
+    age = 0
+    tel = ""
+    email = ""
+
+    def lingin(self):
+        print("J'ai besoin de me connecter")
+
+    def logout(self):
+        print("J'ai terminé et j'ai besoin de me déconnecter")
--- exo.py
+++ exo.py
@@ -1,16 +1,2 @@
-# La clause finaly
-def inverse(x):
-    y = 1.0/x
-    return y
+# Définir une classe Python
 
-try:
-    x = float(input("Donnez un nombre que vous souhaitez avoir son inverse:"))
-    y = inverse(x)
-except ZeroDivisionError:
-    print("Impossible de calculer l'inverse de zéro, veuillez entrer un nombre différent de zéro")
-except ValueError:
-    print("La valeur entrée n'est pas un nombre")
-else:
-    print(y)
-finally:
-    print("Le contenu de ce bloc est toujours exécuté!")
