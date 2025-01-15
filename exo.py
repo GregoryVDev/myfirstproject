bb140bdd160f5a1d0c9c208f767e5c671ba35f33
GregoryVDev <arexo@hotmail.fr>
2025-01-15 18:49:01 +0100

    Added the commentary for the private attributes

--- exo.py
+++ exo.py
@@ -1,10 +1,13 @@
 # L'encapsulation et les attributs privés
 class Person:
     def __init__(self,name,fonction):
-        self.name = name
+        self.person_name = name
         self.fonction = fonction
 
 person1 = Person("Smith", "Développeur web")
 
-print("Nom:", person1.name)
+print("Nom:", person1.person_name)
 print("Fonction:", person1.fonction)
+
+
+# Déclarer un attribut privé avec "__"
