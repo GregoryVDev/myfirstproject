aad7b549db307404c235f97c7d26192a147b38b9
GregoryVDev <arexo@hotmail.fr>
2025-01-16 20:33:43 +0100

    Displayed if the letter ‘r’ exists in person1 (name and function), it exists, then it returns true (case sensitive)

--- Person.py
+++ Person.py
@@ -6,7 +6,7 @@ class Person:
 
     # Permet de retourner si l'élément item est dans le person_name
     def __contains__(self, item):
-        return item in self.person_name
+        return item in self.person_name or self.person_fonction
 
 person1 = Person("Mastafi", "Professeur")
 
