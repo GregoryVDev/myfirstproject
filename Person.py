701479ef02a8c3a44544674a4500e75ab6131625
GregoryVDev <arexo@hotmail.fr>
2025-01-16 17:10:45 +0100

    Added the __add__ function with the other attribute to be able to add the name of person 1 with self and the name of person 2 with other to the format.

--- Person.py
+++ Person.py
@@ -3,9 +3,15 @@ class Person:
         self.person_name = name
         self.person_fonction = fonction
 
+    def __add__(self, other):
+        return "Je suis l'objet {}, je peux ajouter l'objet {}".format(self.person_name, other.person_name)
+
+
 
 person1 = Person("Mastafi", "Professeur")
 print(person1.person_name, person1.person_fonction)
 
 person2 = Person("Smith", "Etudiante")
 print(person2.person_name, person2.person_fonction)
+
+print(person1 + person2)
