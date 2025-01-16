68e5bf55c7ebb5c9a55c8fe4dbf1b5fedab2e80f
GregoryVDev <arexo@hotmail.fr>
2025-01-16 14:38:45 +0100

    Displayed agent1's information and function (simple inheritance)

--- Employe.py
+++ Employe.py
@@ -1,4 +1,4 @@
-# Le constructeur
+# L'héritage simple
 class Employe:
     def __init__(self, name, firstname, age, email):
         self.employe_name = name
@@ -12,8 +12,12 @@ class Employe:
     def logout(self):
         print("J'ai besoin de me déconnecter")
 
-employe1 = Employe("Smith", "Joe", 40, "joe@gmail.fr")
-print(employe1.employe_name, employe1.employe_firstname, "a pour âge", employe1.employe_age, "ans", "et son adresse email", employe1.employe_email)
 
-employe1.login()
-employe1.logout()
+class Human_resources(Employe):
+    fonction = "Les agents de ressources humaines gèrent le personnel de l'entreprise"
+
+
+agent1 = Human_resources("Eric", "Frederic", 35, "eric@gmail.com")
+print(agent1.employe_name, agent1.employe_firstname, "a pour âge:", agent1.employe_age, "ans et a pour mail:", agent1.employe_email)
+
+print("Fonction:", agent1.fonction)
--- exo.py
+++ exo.py
@@ -1,19 +1,2 @@
-# Les accesseurs et les mutateurs (getters et setters)
-class Person:
-    def __init__(self,name,fonction):
-        # Déclarer un attribut privé avec "__"
-        self.__person_name = name
-        self.fonction = fonction
 
-    def getName(self):
-        return self.__person_name
 
-    def setName(self, name ):
-        self.__person_name = name
-
-person1 = Person("Smith", "Développeur web")
-
-print("Nom:", person1.getName())
-
-person1.setName("Dupond")
-print("Nom:", person1.getName())
