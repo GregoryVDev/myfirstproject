2a4c4005076be8e97955e78e10a0f697d6b65e12
GregoryVDev <arexo@hotmail.fr>
2025-01-16 17:27:18 +0100

    Added the __str__ function to display the name and function of person1

--- Person.py
+++ Person.py
@@ -1,18 +1,13 @@
-# La méthode spéciale __add__
+# La méthode spéciale __str__
 class Person:
     def __init__(self, name, fonction):
         self.person_name = name
         self.person_fonction = fonction
 
-    def __add__(self, other):
-        return "Je suis l'objet {}, je peux ajouter l'objet {}".format(self.person_name, other.person_name)
+    def __str__(self):
+        return "Mon nom est {} et ma fonction est {}".format(self.person_name, self.person_fonction)
 
 
 
 person1 = Person("Mastafi", "Professeur")
-print(person1.person_name, person1.person_fonction)
-
-person2 = Person("Smith", "Etudiante")
-print(person2.person_name, person2.person_fonction)
-
-print(person1 + person2)
+print(person1)
