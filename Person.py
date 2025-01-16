26a2a8f5189dace4084f8522e4d7d0cbad01c58a
GregoryVDev <arexo@hotmail.fr>
2025-01-16 20:36:00 +0100

    Displayed if the letter ‘r’ exists in person1 (name and function), if it exists in one of the two, it returns false, if it exists in both, it returns true

--- Person.py
+++ Person.py
@@ -6,8 +6,8 @@ class Person:
 
     # Permet de retourner si l'élément item est dans le person_name
     def __contains__(self, item):
-        # Vérifie si la lettre "r" existe ou dans le nom  ou dans la fonction
-        return item in self.person_name or self.person_fonction
+        # Vérifie si la lettre "r" existe et dans le nom et dans la fonction pour renvoyer true, il faut que la lettre demandé soit sur les deux mots (nom et fonction)
+        return item in self.person_name and self.person_fonction
 
 person1 = Person("Mastafi", "Professeur")
 
