8959e6dfc109b04c3fd0325f484e017a7369c7e5
GregoryVDev <arexo@hotmail.fr>
2025-01-16 20:15:06 +0100

    Created a function with the __len__ method with print and return the number of letters (7) in the name (Mastafi)

--- Person.py
+++ Person.py
@@ -1,13 +1,12 @@
-# La méthode spéciale __str__
+# La méthode spéciale __len__
 class Person:
     def __init__(self, name, fonction):
         self.person_name = name
         self.person_fonction = fonction
 
-    def __str__(self):
-        return "Mon nom est {} et ma fonction est {}".format(self.person_name, self.person_fonction)
-
-
+    def __len__(self):
+        print("C'est une formation en Python")
+        return len(self.person_name)
 
 person1 = Person("Mastafi", "Professeur")
-print(person1)
+print(len(person1))
