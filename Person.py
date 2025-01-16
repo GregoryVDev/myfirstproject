d5c70bbef0ef2bc61d9a8097b71e386ba645e90a
GregoryVDev <arexo@hotmail.fr>
2025-01-16 17:04:07 +0100

    Displayed with person2 name and function

--- Person.py
+++ Person.py
@@ -6,3 +6,6 @@ class Person:
 
 person1 = Person("Mastafi", "Professeur")
 print(person1.person_name, person1.person_fonction)
+
+person2 = Person("Smith", "Etudiante")
+print(person2.person_name, person2.person_fonction)
